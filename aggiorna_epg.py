d79e5152f2116cd98403d0c861a8dc33677bf0f9
Domenico <fjs2w772my@privaterelay.appleid.com>
2025-11-19 21:16:47 +0100

    Aggiornato script e generata EPG combinata

--- aggiorna_epg.py
+++ aggiorna_epg.py
@@ -1,17 +1,44 @@
 import requests
+import lzma
+import xml.etree.ElementTree as ET
 
-# URL EPG Rytec (Sky Italia)
-URL = "http://www.xmltvepg.nl/rytecIT_Sky.xz"
-OUTPUT = "epg.xz"
+# URL dei feed Rytec
+feeds = {
+    "Sky": "http://www.xmltvepg.nl/rytecIT_Sky.xz",
+    "Basic": "http://www.xmltvepg.nl/rytecIT_Basic.xz",
+    "SportMovies": "http://www.xmltvepg.nl/rytecIT_SportMovies.xz"
+}
 
-print("Scarico l'EPG Rytec Italia Sky...")
+# Lista per salvare tutti gli eventi
+all_programs = []
 
-try:
-    r = requests.get(URL)
-    r.raise_for_status()
-    with open(OUTPUT, "wb") as f:
-        f.write(r.content)
-    print("Download completato: epg.xz")
-except Exception as e:
-    print("Errore durante il download:", e)
+for name, url in feeds.items():
+    print(f"Scarico feed {name}...")
+    resp = requests.get(url)
+    resp.raise_for_status()
+    # Decomprimi l'xz
+    decompressed = lzma.decompress(resp.content)
+    # Parse XML
+    root = ET.fromstring(decompressed)
+    # Aggiungi tutti gli eventi alla lista
+    for program in root.findall("programme"):
+        all_programs.append(program)
+
+# Creiamo il nuovo XML combinato
+root_combined = ET.Element("tv")
+
+for program in all_programs:
+    root_combined.append(program)
+
+tree = ET.ElementTree(root_combined)
+
+# Salviamo in epg.xml temporaneo
+tree.write("epg.xml", encoding="utf-8")
+
+# Comprimiamo in xz
+with lzma.open("epg.xz", "wb") as f:
+    with open("epg.xml", "rb") as infile:
+        f.write(infile.read())
+
+print("EPG combinata salvata in epg.xz")
 
